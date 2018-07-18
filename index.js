368f7423302ea0999e2b4793e51c1746e447a43f
LukeEllul <ellul.luke@gmail.com>
2018-07-18 18:44:48 +0200

    added some more code...project is finished!!!

--- index.js
+++ index.js
@@ -6,80 +6,50 @@ const { getPhero } = require('./src/components/Edge');
 const { getTourLength, getBestAnt, getPreviousNodes } = require('./src/components/Ant');
 
 const { berlin } = require('./src/data/berlin');
+const { cities107 } = require('./src/data/107cities');
+const { st70 } = require('./src/data/st70');
 
-const Graph = R.pipe(
+const BerlinGraph = R.pipe(
     convertToMap,
     makeGraphMatrix,
     getEdges,
     newGraph
 )(berlin);
 
+const cities107Graph = R.pipe(
+    convertToMap,
+    makeGraphMatrix,
+    getEdges,
+    newGraph
+)(cities107);
+
+const st70Graph = R.pipe(
+    convertToMap,
+    makeGraphMatrix,
+    getEdges,
+    newGraph
+)(st70);
+
 const x = Graph => new Promise(res =>
     completeTour(
-        0.8,
-        4,
-        7,
-        0.7,
+        0.7, //q
+        4, //B
+        7, //amount of pheronome
+        0.7, //evaporation rate
         placeAnts(R.range(0, nodesNumber(Graph))),
         Graph,
         updatedAnts => updatedGraph =>
-            (x => console.log(x) || x)(getTourLength(getBestAnt(updatedAnts))) < 9000 ? res(getPreviousNodes(getBestAnt(updatedAnts))) :
+            (x => console.log(x) || x)(getTourLength(getBestAnt(updatedAnts))) < 800 /*change this to stop iteration*/ ? res(getPreviousNodes(getBestAnt(updatedAnts))) :
                 setTimeout(_ => x(updatedGraph).then(res), 1)
     ));
 
-x(Graph).then(console.log)
+//uncomment the statement of your choice
 
-//lowest point:
-//9001.23
-// [ 44,
-//     18,
-//     40,
-//     7,
-//     8,
-//     9,
-//     42,
-//     14,
-//     5,
-//     4,
-//     23,
-//     47,
-//     36,
-//     37,
-//     38,
-//     39,
-//     35,
-//     34,
-//     33,
-//     48,
-//     31,
-//     0,
-//     21,
-//     17,
-//     30,
-//     20,
-//     16,
-//     2,
-//     22,
-//     28,
-//     45,
-//     24,
-//     3,
-//     50,
-//     11,
-//     27,
-//     26,
-//     25,
-//     46,
-//     12,
-//     13,
-//     51,
-//     10,
-//     32,
-//     43,
-//     49,
-//     19,
-//     15,
-//     29,
-//     41,
-//     6,
-//     1 ]
+//berlin
+//x(BerlinGraph).then(console.log)
+
+//107 cities
+//x(cities107Graph).then(console.log);
+
+//70 locations
+//x(st70Graph).then(console.log);
